6bb7431ae728e3d68b1597409d4d7adc95043fba
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2024-03-18 23:36:48 +0800

    refactor(postCreateCommand.sh): refactor path adding part

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -18,10 +18,8 @@ latest_texlive_path="$(ls -d /usr/local/texlive/[0-9]* | sort -n | tail -1)/bin/
 echo "export PATH=\"$latest_texlive_path:\$PATH\"" >> ~/.bashrc
 echo "export PATH=\"$latest_texlive_path:\$PATH\"" >> ~/.zshrc
 echo "export PATH=\"$latest_texlive_path:\$PATH\"" >> ~/.profile
+sudo env PATH=$PATH tlmgr path add
 
 # USERMODE init & Install common TeX Live packages
 # sudo apt install texlive-guesswhich -y
 tlmgr init-usertree
-tlmgr --usermode install biblatex fontspec microtype geometry hyperref graphicx xcolor cleveref babel tabularx caption enumitem tikz pgf xecjk
-sudo env PATH=$PATH tlmgr install latexmk latexindent chktex kpsewhich lacheck synctex texcount texdoc
-sudo env PATH=$PATH tlmgr path add
